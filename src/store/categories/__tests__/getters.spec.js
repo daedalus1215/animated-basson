4ab11448f22c906c84c3bb19081299e537a542da
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-01-18 21:34:54 -0500

    add test for getters

--- src/store/categories/__tests__/getters.spec.js
+++ src/store/categories/__tests__/getters.spec.js
@@ -1,7 +1,18 @@
+import getters from '../getters';
+
 describe('store/categories/__tests__/getters.spec.js', () => {
-    describe('eee', () => {
-        it('check', () => {
-            expect(true).toEqual(true);
+    describe('getListOfCategories', () => {
+        it('should update state.listOfCategories with localStorage', () => {
+            const expected = { listOfCategories: { 'item': 'item' } };
+            Object.defineProperty(window, 'localStorage', {
+                value: {
+                    getItem: () => { return JSON.stringify({ 'item': 'item' }) }
+                },
+            });
+
+            const actual = getters.getListOfCategories({});
+            
+            expect(actual).toEqual(expected);
         });
     });
 });
