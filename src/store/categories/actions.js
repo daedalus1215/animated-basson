618a7d225f06a71f0a4a8ce519051931e2327d19
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-03-24 11:45:09 -0400

    Add updateTodoCheckbox for actions.js Also add a comment as to why we are comfortable with actions.js being one file and contrast it with the more files for mutations - for this domain.

--- src/store/categories/actions.js
+++ src/store/categories/actions.js
@@ -1,3 +1,12 @@
+/**
+ * Actions are essentially just registrations of mutations.
+ * 
+ * So it seems ok to have one registry file (actions) for this
+ * particular domain (categories). Whereas, with mutations, we have individual 
+ * mutation files because there is so much logic in each one of the mutations.
+ * It seemed in the best interest of cognitive load to carve them out individually 
+ * opposed to what we are doing with actions, which is one file (for domain). 
+ */
 export default {
     addToListOfCategories(context, category) {
         context.commit('addToListOfCategories', category);
@@ -7,6 +16,9 @@ export default {
     },
     deleteTodoInCategory(context, todoIdWithCategoryId) {
         context.commit('deleteTodoInCategory', todoIdWithCategoryId);
+    },
+    updateTodoCheckboxStatus(context, todoIdWithCategoryidAndStatus) {
+        context.commit('updateTodoCheckboxStatus', todoIdWithCategoryidAndStatus)
     },
     deleteCategory(context, categoryId){
         context.commit('deleteCategory', categoryId);
